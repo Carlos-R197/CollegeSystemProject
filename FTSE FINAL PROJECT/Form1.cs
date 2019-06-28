5908b906452f90a69406cdaadf0b7be8315470fb
John Marcos Espinal Hernandez <john-espinal10@hotmail.com>
2019-06-27 23:51:58 -0400

    FINAL PROJECT COMMIT 2

--- FTSE FINAL PROJECT/Form1.cs
+++ FTSE FINAL PROJECT/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CrystalDecisions.CrystalReports;
 
 namespace FTSE_FINAL_PROJECT
 {
@@ -17,5 +18,7 @@ namespace FTSE_FINAL_PROJECT
             InitializeComponent();
         }
 
+        
+        
     }
 }
